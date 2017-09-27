ffc54476d2024b281b9b7f29822ccdac7f91c497
Alexander Egorichev <alexchev92@gmail.com>
2017-09-27 17:02:22 +0300

    разметка works и welcome

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const webpack = require('webpack');
+
+module.exports = {
+  babelrc: true,
+  output: {
+    filename: 'bundle.min.js',
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+    }]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+      },
+      output: {
+        comments: false,
+      },
+    }),
+  ]
+};
